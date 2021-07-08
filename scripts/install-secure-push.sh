dca4c37e7628e106c7c7497cbaff90edb2927815
suryatech27-cloud <sunil.techno27@gmail.com>
2021-07-08 16:03:46 +0530

    plugin name changed to signed-push

--- scripts/install-secure-push.sh
+++ scripts/install-secure-push.sh
@@ -27,19 +27,19 @@ DIR=$(helm env HELM_PLUGINS)
 
 if [ $OS == "windows" ]; then
         #echo $HELM_PLUGIN_DIR
-        cd $DIR'/helm-secure-push/bin'; rm 'securepush';
+        cd $DIR'/signed-push/bin'; rm 'securepush';
         #echo $PUSH_BIN_PATH
 
         #cd $DIR'/helm-secure-push/bin'; env GOOS=windows GOARCH=amd64 go build ../cmd/securepush
-        cd 'C:/Windows/System32'; setx path "$DIR\helm-secure-push\bin"
+        cd 'C:/Windows/System32'; setx path "$DIR\signed-push\bin"
 elif [ $OS == "linux" ]; then
-  cd $DIR'/helm-secure-push/bin'; rm 'securepush.exe';
-  echo 'export PATH=$PATH:'$DIR'/helm-secure-push/bin' >> ~/.bashrc
+  cd $DIR'/signed-push/bin'; rm 'securepush.exe';
+  echo 'export PATH=$PATH:'$DIR'/signed-push/bin' >> ~/.bashrc
   source ~/.bashrc
   echo "Installed Successfully Done."
 else 
-  cd $DIR'/helm-secure-push/bin'; rm 'securepush.exe';
-  echo 'export PATH=$PATH:'$DIR'/helm-secure-push/bin' >> ~/.bashrc
+  cd $DIR'/signed-push/bin'; rm 'securepush.exe';
+  echo 'export PATH=$PATH:'$DIR'/signed-push/bin' >> ~/.bashrc
   source ~/.bashrc
   echo "Installed Successfully Done."
 fi
